0710d928ef66f86278e8d7020594ddbc8d8202df
BryanTorresCalle <50812831+BryanTorresCalle@users.noreply.github.com>
2021-10-04 10:52:08 -0500

    Get person by uuid added

--- router/index.js
+++ router/index.js
@@ -8,7 +8,7 @@ router.post('/materials', materialHandler.newMaterial)
 router.post('/persons', personHandler.newPerson)
 router.get('/persons', personHandler.getAllPersons)
 router.put('/persons', personHandler.updatePersons)
-
+router.get('/persons/:uuid', personHandler.getPersonByUuid)
 
 
 module.exports = router
