ddc6d080c475d9878479347044cb8a66d2124be5
Carlangasv <50808838+Carlangasv@users.noreply.github.com>
2021-10-04 18:39:52 -0500

    Update 1 work Delete 1 work Get 1 work Done.

--- handler/works/index.js
+++ handler/works/index.js
@@ -1,3 +1,4 @@
+const e = require("express");
 const { uuid } = require("uuidv4");
 const _db = require("../../services/db");
 
@@ -45,7 +46,7 @@ const newWork = (req, res) => {
   }
 };
 
-const getAllWorks = (req, res) => {
+const getWorks = (req, res) => {
   try {
     let data = _db.getData("works");
     let dataToReturn = Array.from(data);
@@ -65,41 +66,149 @@ const getAllWorks = (req, res) => {
 };
 
 const updateWorks = (req, res) => {
-    try {
-        let body = req.body.list
-        for (let item of body) {
-            if (item[1].id) {
-                res.status(400).send({
-                    success: false,
-                    message: "Can't update this work ",
-                    error: `Can't change the value of the key: 'id'`
-                })
-                return
-            }
-        }
-        let data = _db.getData('works')
-        let countUpdated = 0
-        for (let item of body) {
-            if (data.has(item[0])) {
-                let oldItem = data.get(item[0])
-                data.set(item[0], {...oldItem, ...item})
-                countUpdated++
-            }
-        }
-        _db.writeDB(data, 'works')
-        res.status(200).send({
-            success: true,
-            message: "Works updated successfully",
-            info: `Number of items updated: ${countUpdated}`
-        })
-    } catch (error) {
-        console.error(error)
-        res.status(500).send({
-            success: false, 
-            message: "There was an error trying to update works.",
-            error: error.message
-        })
+  try {
+    let body = req.body.list;
+    for (let item of body) {
+      if (item[1].id) {
+        res.status(400).send({
+          success: false,
+          message: "Can't update this work ",
+          error: `Can't change the value of the key: 'id'`,
+        });
+        return;
+      }
     }
-}
+    let data = _db.getData("works");
+    let countUpdated = 0;
+    for (let item of body) {
+      if (data.has(item[0])) {
+        let oldItem = data.get(item[0])[1];
+        data.set(item[0], { ...oldItem, ...item[1] });
+        countUpdated++;
+      }
+    }
+    _db.writeDB(data, "works");
+    res.status(200).send({
+      success: true,
+      message: "Works updated successfully",
+      info: `Number of items updated: ${countUpdated}`,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      success: false,
+      message: "There was an error trying to update works.",
+      error: error.message,
+    });
+  }
+};
 
-module.exports = { newWork, getAllWorks, updateWorks};
+const getWork = (req, res) => {
+  try {
+    let body = req.body;
+    let uuid = req.params.uuid;
+    let data = _db.getData("works");
+    let exists = _db.has(data, "id", body.id);
+    if (exists) {
+      let info = data.get(uuid);
+      res.status(200).send({
+        success: true,
+        message: "Work found succesfully",
+        info,
+      });
+    } else {
+      res.status(200).send({
+        success: true,
+        message: "Work failed to found",
+        info: "Work does not exist",
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      success: false,
+      message: "There was an error trying to get the work.",
+      error: error.message,
+    });
+  }
+};
+
+const updateWork = (req, res) => {
+  try {
+    let body = req.body;
+    let uuid = req.params.uuid;
+    if (body.id) {
+      res.status(400).send({
+        success: false,
+        message: "Can't update this work ",
+        error: `Can't change the value of the key: 'id'`,
+      });
+      return;
+    }
+    let data = _db.getData("works");
+    let item = req.body;
+    let oldItem = data.get(uuid);
+    if (oldItem) {
+      data.set(uuid, { ...oldItem, ...item });
+      _db.writeDB(data, "works");
+      res.status(200).send({
+        success: true,
+        message: "Works updated successfully",
+        info: `Item updated ${uuid}`,
+      });
+    } else {
+      res.status(400).send({
+        success: true,
+        message: "Can not update this item",
+        info: `Item ${uuid} does not exist`,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      success: false,
+      message: "There was an error trying to update works.",
+      error: error.message,
+    });
+  }
+};
+
+const deleteWork = (req, res) => {
+  try {
+    let uuid = req.params.uuid;
+    let data = _db.getData("works");
+    let item = req.body;
+    let oldItem = data.get(uuid);
+    if (oldItem) {
+      data.delete(uuid);
+      _db.writeDB(data, "works");
+      res.status(200).send({
+        success: true,
+        message: "Work deleted successfully",
+        info: `Work deleted ${uuid}`,
+      });
+    } else {
+      res.status(400).send({
+        success: true,
+        message: "Can not delete this item",
+        info: `Work ${uuid} does not exist`,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      success: false,
+      message: "There was an error trying to delete this work.",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = {
+  newWork,
+  getWorks,
+  updateWorks,
+  getWork,
+  updateWork,
+  deleteWork,
+};
--- router/index.js
+++ router/index.js
@@ -1,10 +1,13 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const workHandler = require("../handler/works")
+const workHandler = require("../handler/works");
 
-router.get('/works', workHandler.getAllWorks)
-router.post('/works', workHandler.newWork)
-router.put('/works', workHandler.updateWorks)
+router.get("/works", workHandler.getWorks);
+router.post("/works", workHandler.newWork);
+router.put("/works", workHandler.updateWorks);
+router.get("/works/:uuid", workHandler.getWork);
+router.put("/work/:uuid", workHandler.updateWork);
+router.delete("/work/:uuid", workHandler.deleteWork);
 
-module.exports = router
+module.exports = router;
