9ebe8f5f0de87a55f786880749735c5543c52dab
Carlangasv <50808838+Carlangasv@users.noreply.github.com>
2021-10-05 07:38:20 -0500

    Documentation

--- handlers/uploads/index.js
+++ handlers/uploads/index.js
@@ -2,6 +2,12 @@ const { uuid } = require("uuidv4");
 const fs = require("fs");
 const { saveFile } = require("../../services/fileManager");
 
+/** Upload any tipe of file
+ * @param {Object} req
+ * @param {Object[]} req.files
+ * @param {Object} req.files.file
+ * @param {string} req.files.file.name
+ */
 const uploadFile = (req, res) => {
   let newUuid = uuid();
   let ext = req.files.file.name.split(".")[1];
@@ -15,10 +21,25 @@ const uploadFile = (req, res) => {
   });
 };
 
+/** Get a file by the name and download it
+ * @param {Object} req
+ * @param {Object} req.params
+ * @param {Object} req.params.name
+ */
 const getFile = (req, res) => {
   let name = req.params.name;
-  console.log(name);
-  var readStream = fs.createReadStream("./data/files/" + name);
-  readStream.pipe(res, {ok : true});
+  let path = "./data/files/" + name;
+  try {
+    fs.readFile(path, function (err, data) {
+      if (err) {
+        res.status(500).send({Ok: false, message: "The file does not exist"});
+      } else {
+        var readStream = fs.createReadStream(path);
+        readStream.pipe(res);
+      }
+    });
+  } catch (error) {
+    res.status(500).send({ error });
+  }
 };
 module.exports = { uploadFile, getFile };
