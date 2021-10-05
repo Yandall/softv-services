573e29c732b9da70599d64a418eb349782a0f169
Carlangasv <50808838+Carlangasv@users.noreply.github.com>
2021-10-04 19:19:00 -0500

    Documentation

--- handler/works/index.js
+++ handler/works/index.js
@@ -1,11 +1,17 @@
-const e = require("express");
 const { uuid } = require("uuidv4");
 const _db = require("../../services/db");
 
+/** Creates a new work, the params are:
+ * @param {Object} req
+ * @param {Object} req.body the body of the request must contain the following params
+ * @param {string} req.body.id unique id of the new work
+ * @param {string} req.body.address the address of the work, does not need an special formating
+ * @param {string} req.body.name the name of the work.
+ */
 const newWork = (req, res) => {
   try {
     let body = req.body;
-    if (!body.name || !body.id || !body.addres) {
+    if (!body.name || !body.id || !body.address) {
       res.status(400).send({
         success: false,
         message: "Please provide all the needed fields.",
@@ -27,7 +33,7 @@ const newWork = (req, res) => {
     let newItem = {
       name: body.name,
       id: body.id,
-      addres: body.addres,
+      address: body.address,
     };
     data.set(newUuid, newItem);
     _db.writeDB(data, "works");
@@ -46,6 +52,7 @@ const newWork = (req, res) => {
   }
 };
 
+/**Returns all the created works */
 const getWorks = (req, res) => {
   try {
     let data = _db.getData("works");
@@ -65,6 +72,12 @@ const getWorks = (req, res) => {
   }
 };
 
+/** Update multiple works, the params are the following:
+@param {Object} req
+@param {Object[]} req.list array that contains the works to update, each work must be an array
+@param {string} req.list[].uuid an uuid that exists
+@param {JSON} req.list[].data the new data of each object, this data can not contain the id, this value can not be updated
+*/
 const updateWorks = (req, res) => {
   try {
     let body = req.body.list;
@@ -102,15 +115,17 @@ const updateWorks = (req, res) => {
     });
   }
 };
-
+/** Return a single work
+ * @param {Object} req 
+ * @param {Object} req.params parameteres
+ * @param {string} req.params.uuid uuid to search
+*/
 const getWork = (req, res) => {
   try {
-    let body = req.body;
     let uuid = req.params.uuid;
     let data = _db.getData("works");
-    let exists = _db.has(data, "id", body.id);
-    if (exists) {
-      let info = data.get(uuid);
+    let info = data.get(uuid);
+    if (info) {
       res.status(200).send({
         success: true,
         message: "Work found succesfully",
@@ -132,7 +147,12 @@ const getWork = (req, res) => {
     });
   }
 };
-
+/** Update a single work
+ * @param {Object} req 
+ * @param {Object} req.body values to update, can not contain id
+ * @param {Object} req.params parameteres
+ * @param {string} req.params.uuid uuid to search
+*/
 const updateWork = (req, res) => {
   try {
     let body = req.body;
@@ -172,12 +192,15 @@ const updateWork = (req, res) => {
     });
   }
 };
-
+/** Delete a single work
+ * @param {Object} req 
+ * @param {Object} req.params parameteres
+ * @param {string} req.params.uuid uuid to delet
+*/
 const deleteWork = (req, res) => {
   try {
     let uuid = req.params.uuid;
     let data = _db.getData("works");
-    let item = req.body;
     let oldItem = data.get(uuid);
     if (oldItem) {
       data.delete(uuid);
--- router/index.js
+++ router/index.js
@@ -3,10 +3,13 @@ const router = express.Router();
 
 const workHandler = require("../handler/works");
 
+/*Routing to works in lots*/
 router.get("/works", workHandler.getWorks);
 router.post("/works", workHandler.newWork);
 router.put("/works", workHandler.updateWorks);
-router.get("/works/:uuid", workHandler.getWork);
+
+/*Routing to a single work */
+router.get("/work/:uuid", workHandler.getWork);
 router.put("/work/:uuid", workHandler.updateWork);
 router.delete("/work/:uuid", workHandler.deleteWork);
 
